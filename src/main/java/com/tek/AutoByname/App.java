ef843015a0835ddc782185f8fb826c3b72d9433c
Aj <magantiajay143@gmail.com>
2017-06-01 12:52:35 -0400

    mag1

---
+++ src/main/java/com/tek/AutoByname/App.java
@@ -0,0 +1,17 @@
+package com.tek.AutoByname;
+
+
+	import org.springframework.context.ApplicationContext;
+	import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+	public class App {
+		public static void main(String[] args) {
+			ApplicationContext context = new ClassPathXmlApplicationContext(
+					"NewFile.xml");
+
+			Customer cust = (Customer) context.getBean("customer");
+			System.out.println(cust);
+		}
+	}
+
+
